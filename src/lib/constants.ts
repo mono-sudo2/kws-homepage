7e971c8b6ded4b35dba30ee6dde4cc150745943b
Emanuel Rahn <emanuelrahn@rahn-tech.de>
2026-03-20 18:52:00 +0100

    constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,3 @@
 export const BOOKING_URL =
-  process.env.NEXT_PUBLIC_BOOKING_URL ?? "https://buchung.kletterwald-staufen.de";
+	process.env.NEXT_PUBLIC_BOOKING_URL ??
+	"https://booking.touritickets.de/book/07320856-dcc2-42cf-91f2-09c8e789ebb0/calendar?date=2026-03-28";
